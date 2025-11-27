2f3ff2c8508527fc659636e883d02d607607d1a3
Yuma Hosozawa <yuma.sakahiga@gmail.com>
2025-11-28 00:19:17 +0900

    fix:条件式の修正およびデバッグ用の関数呼び出しの修正

--- components/moduleA.py
+++ components/moduleA.py
@@ -2,12 +2,16 @@
 def menu():
     i=0
     function =["乗車選択","チャージ機能"]
+    option_num = int(len(function))
+
     print("【ウチダ電鉄 交通系ICカード検証システム】")
-    for f in function:
-        print(str(i+1) + ":" + f)
+    for i in range(option_num):
+        print(str(i+1) + ":" + function[i])
         i=i+1
     print("使用する機能を入力してください(終了する場合は99を入力)")
     func_num=int(input())
     return func_num
 
-menu()
+if __name__ == "__main__":
+    menu()
+    
