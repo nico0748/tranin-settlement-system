28f6b2920de3604180eadffe940db5300641f941
HR0101 <hararyuto0101@gmail.com>
2025-11-27 12:10:59 +0900

    update: charge関数の実装

--- components/moduleD.py
+++ components/moduleD.py
@@ -1,1 +1,20 @@
 # charge関数
+
+def charge(charge_balance):
+    charges = []
+    for i in range(10):
+        charges.append((i + 1) * 1000)
+    print("【チャージ機能】")
+    print(f"チャージ残高は{charge_balance}円です。")
+    for i in range(len(charges)):
+        print(f"{i + 1}：{charges[i]}円")
+    print("チャージする金額を選択してください。(キャンセルする場合には 99 を入力)")
+    choice = int(input())
+    if 1 <= choice <= 10:
+        charge_amount = choice * 1000
+        print(f"{charge_amount}円チャージします。")
+        charge_balance += charge_amount
+    elif choice == 99:
+        print("チャージをキャンセルしました。")
+    print(f"チャージ残高は{charge_balance}円です。")
+    return charge_balance
