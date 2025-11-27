3127db68f2b92cd2c02692b2409bf28b9257ade9
Yuma Hosozawa <yuma.sakahiga@gmail.com>
2025-11-28 00:24:36 +0900

    feat: pay関数を実装

--- components/moduleC.py
+++ components/moduleC.py
@@ -1,1 +1,24 @@
 # pay関数
+def pay(charge_balance, destination):
+    stations = ["秋葉原", "山梨", "長野"]
+    fares = [133, 4128, 7990]
+
+    print("チャージ残高は"+str(charge_balance)+"円です")
+
+    if charge_balance < 500:
+        print("残高が500 円未満のため3000 円自動チャージします。")
+        charge_balance += 3000
+        print("チャージ残高は"+str(charge_balance)+"円です。")
+    
+    if fares[destination] > charge_balance:
+        while charge_balance <= fares[destination]:
+            print("残高不足です")
+            print("3000円自動チャージします。")
+            charge_balance += 3000
+    
+    charge_balance -= fares[destination]
+    print("精算後のチャージ残高は"+str(charge_balance)+"円です。")
+    
+    return charge_balance
+            
+
