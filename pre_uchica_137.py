236eee81e3e8d6c685269d2031ebd40c24ae3cb7
Yuma Hosozawa <yuma.sakahiga@gmail.com>
2025-11-28 00:25:22 +0900

    add: 各機能を結合したプログラムを追加

---
+++ pre_uchica_137.py
@@ -0,0 +1,32 @@
+from components import moduleA as ma
+from components import moduleB as mb
+from components import moduleC as mc
+from components import moduleD as md
+
+def main():
+    # チャージ金額の初期値は500
+    charge_balance = 500
+    user_input = "0"
+
+    while True:
+        try:
+            if user_input == "q":
+                print("プログラムを終了します。")
+                break # ループを抜けて終了する
+
+            menu_num = ma.menu()
+            if menu_num == 1 :
+                destination = mb.select_station()
+                mc.pay(charge_balance, destination)
+            elif menu_num ==2 :
+                charge_balance = md.charge(charge_balance)
+            elif menu_num == 99:
+                continue
+        except:
+            print("有効な数値を入力し直してください。")
+            continue
+            
+
+
+if __name__ == "__main__":
+    main()
--- uchica_137.py
+++
@@ -1,8 +0,0 @@
-from components import moduleA as ma
-from components import module as mb
-
-def main():
-    return
-
-if __name__ == "__main__":
-    main()
